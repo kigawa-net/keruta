ebeee492507c3dfe8a0d5310d1c727591c50a82a
kigawa <mail@kigawa.net>
2025-06-18 11:03:45 +0900

    wp

--- infra/persistence/src/main/kotlin/net/kigawa/keruta/infra/persistence/config/MongoConfig.kt
+++ infra/persistence/src/main/kotlin/net/kigawa/keruta/infra/persistence/config/MongoConfig.kt
@@ -4,6 +4,7 @@ import com.mongodb.ConnectionString
 import com.mongodb.MongoClientSettings
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
@@ -12,12 +13,32 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 @EnableMongoRepositories(basePackages = ["net.kigawa.keruta.infra.persistence.repository"])
 class MongoConfig : AbstractMongoClientConfiguration() {
 
+    @Value("\${spring.data.mongodb.host}")
+    private lateinit var host: String
+
+    @Value("\${spring.data.mongodb.port}")
+    private lateinit var port: String
+
+    @Value("\${spring.data.mongodb.database}")
+    private lateinit var database: String
+
+    @Value("\${spring.data.mongodb.username}")
+    private lateinit var username: String
+
+    @Value("\${spring.data.mongodb.password}")
+    private lateinit var password: String
+
+    @Value("\${spring.data.mongodb.authentication-database}")
+    private lateinit var authSource: String
+
     override fun getDatabaseName(): String {
-        return "keruta"
+        return database
     }
 
     override fun mongoClient(): MongoClient {
-        val connectionString = ConnectionString("mongodb://admin:password@localhost:27017/keruta?authSource=admin")
+        val connectionString = ConnectionString(
+            "mongodb://$username:$password@$host:$port/$database?authSource=$authSource"
+        )
 
         val clientSettings = MongoClientSettings.builder()
             .applyConnectionString(connectionString)
