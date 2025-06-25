3b3a91283c7e9affb2fc3c27b7c580b6b89eb9ce
kigawa <mail@kigawa.net>
2025-06-25 09:13:23 +0900

    ConfigMap存在確認処理を追加し、存在しない場合はデフォルト値を空文字列に設定

--- infra/app/src/main/kotlin/net/kigawa/keruta/infra/app/kubernetes/KubernetesInitContainerHandler.kt
+++ infra/app/src/main/kotlin/net/kigawa/keruta/infra/app/kubernetes/KubernetesInitContainerHandler.kt
@@ -115,6 +115,7 @@ class KubernetesInitContainerHandler(
 
     /**
      * Creates an environment variable from a ConfigMap.
+     * If the ConfigMap doesn't exist, it will use an empty string as the default value.
      *
      * @param name The name of the environment variable
      * @param configMapName The name of the ConfigMap
@@ -122,18 +123,36 @@ class KubernetesInitContainerHandler(
      * @return The environment variable
      */
     private fun createConfigMapEnvVar(name: String, configMapName: String, configMapKey: String): EnvVar {
-        val envVar = EnvVar()
-        envVar.name = name
+        // Check if the ConfigMap exists in the namespace
+        val client = clientProvider.getClient()
+        val namespace = clientProvider.getConfig().defaultNamespace
 
-        val valueFrom = EnvVarSource()
-        val configMapKeyRef = io.fabric8.kubernetes.api.model.ConfigMapKeySelector()
-        configMapKeyRef.name = configMapName
-        configMapKeyRef.key = configMapKey
-        configMapKeyRef.optional = true  // Make the ConfigMap reference optional
+        if (client != null) {
+            try {
+                val configMap = client.configMaps().inNamespace(namespace).withName(configMapName).get()
+                if (configMap != null) {
+                    logger.info("ConfigMap $configMapName found, using ConfigMap reference")
+                    val envVar = EnvVar()
+                    envVar.name = name
 
-        valueFrom.configMapKeyRef = configMapKeyRef
-        envVar.valueFrom = valueFrom
+                    val valueFrom = EnvVarSource()
+                    val configMapKeyRef = io.fabric8.kubernetes.api.model.ConfigMapKeySelector()
+                    configMapKeyRef.name = configMapName
+                    configMapKeyRef.key = configMapKey
+                    configMapKeyRef.optional = true  // Make the ConfigMap reference optional
 
-        return envVar
+                    valueFrom.configMapKeyRef = configMapKeyRef
+                    envVar.valueFrom = valueFrom
+
+                    return envVar
+                }
+            } catch (e: Exception) {
+                logger.warn("Error checking for ConfigMap $configMapName: ${e.message}")
+            }
+        }
+
+        // If ConfigMap doesn't exist or there was an error, use an empty string as the default value
+        logger.info("ConfigMap $configMapName not found, using empty string as default value")
+        return EnvVar(name, "", null)
     }
 }
