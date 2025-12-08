04579b76af697e7577453dfb36fa7ec1c2bcd355
kigawa <kigawa.8390@gmail.com>
2025-12-08 12:30:27 +0900

    "SessionApiコードを削除し、関連コンパイルモジュール設定を更新"

--- ktcp/model/src/commonMain/kotlin/net/kigawa/keruta/ktcp/model/KtcpUnknownMsg.kt
+++ ktcp/model/src/commonMain/kotlin/net/kigawa/keruta/ktcp/model/KtcpUnknownMsg.kt
@@ -1,11 +1,6 @@
 package net.kigawa.keruta.ktcp.model
 
 import net.kigawa.keruta.ktcp.model.authenticate.AuthenticateMsg
-import net.kigawa.keruta.ktcp.model.cancel.TaskCancelMsg
-import net.kigawa.keruta.ktcp.model.compleate.TaskCompletedMsg
-import net.kigawa.keruta.ktcp.model.create.TaskCreateMsg
-import net.kigawa.keruta.ktcp.model.heartbeat.HeartbeatMsg
-import net.kigawa.keruta.ktcp.model.message.ErrorMsg
 import kotlin.time.ExperimentalTime
 import kotlin.time.Instant
 
@@ -14,9 +9,4 @@ interface KtcpUnknownMsg {
     val timestamp: Instant
 
     fun tryToAuthenticate(): AuthenticateMsg?
-    fun tryToHeartbeat(): HeartbeatMsg?
-    fun tryToTaskCompleted(): TaskCompletedMsg?
-    fun tryToTaskCancel(): TaskCancelMsg?
-    fun tryToTaskCreate(): TaskCreateMsg?
-    fun tryToError(): ErrorMsg?
 }
