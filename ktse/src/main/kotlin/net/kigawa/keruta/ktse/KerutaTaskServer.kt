29724aebdec62fd02db5cb31ee6ee754d5b8f762
kigawa <mail@kigawa.net>
2025-12-17 18:57:05 +0900

    Implement server authenticate entrypoint for ktse module

---
+++ ktse/src/main/kotlin/net/kigawa/keruta/ktse/KerutaTaskServer.kt
@@ -0,0 +1,10 @@
+package net.kigawa.keruta.ktse
+
+import net.kigawa.keruta.ktcp.server.KtcpServer
+
+object KerutaTaskServer {
+    val serverEntrypoints = KtcpServer()
+    fun module(){
+
+    }
+}
