ba7eccaa7eafc00c68cca85a397d6ba9c30cda22
kigawa <mail@kigawa.net>
2025-12-30 01:47:37 +0900

    デコードエラークラスをktcp/model/serializeに移動し、UnknownArgの認証処理を改善

--- ktcp/model/src/commonMain/kotlin/net/kigawa/keruta/ktcp/model/msg/UnknownArg.kt
+++ ktcp/model/src/commonMain/kotlin/net/kigawa/keruta/ktcp/model/msg/UnknownArg.kt
@@ -3,8 +3,10 @@ package net.kigawa.keruta.ktcp.model.msg
 import net.kigawa.keruta.ktcp.model.authenticate.AuthenticateArg
 import net.kigawa.keruta.ktcp.model.authenticate.AuthenticateMsg
 import net.kigawa.keruta.ktcp.model.err.GenericErrArg
+import net.kigawa.keruta.ktcp.model.serialize.DecodeFrameErr
+import net.kigawa.kodel.api.err.Res
 
 interface UnknownArg {
     fun tryToGenericError(): GenericErrArg?
-    fun tryToAuthenticate(): AuthenticateArg?
+    fun tryToAuthenticate(): Res<AuthenticateArg, DecodeFrameErr>?
 }
