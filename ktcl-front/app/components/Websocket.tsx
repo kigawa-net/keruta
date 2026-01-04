173a31401f7f8a4f2b7880e90cb229974105f04c
kigawa <mail@kigawa.net>
2026-01-05 07:46:24 +0900

    テスト用Docker設定を更新（ktcl-front用の依存関係インストールコマンドを追加）および不要なTS拡張子の削除を実施

--- ktcl-front/app/components/Websocket.tsx
+++ ktcl-front/app/components/Websocket.tsx
@@ -1,5 +1,5 @@
 import {createContext, useContext} from "react";
-import {useWebSocket} from "../hooks/useWebSocket.ts";
+import {useWebSocket} from "../hooks/useWebSocket";
 
 
 const Context = createContext<WebsocketState | undefined>(undefined);
