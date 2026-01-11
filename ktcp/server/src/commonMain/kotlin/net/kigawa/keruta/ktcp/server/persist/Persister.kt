9711fcf202cc648fc1f9a0934abbbe3149937c4e
kigawa <mail@kigawa.net>
2026-01-11 21:26:25 +0900

    ZooKeeperとの統合を追加し、タスク作成用の永続セッション機能を実装。セッションおよび認証に `PersisterSession` を導入してリファクタリングを実施。

--- ktcp/server/src/commonMain/kotlin/net/kigawa/keruta/ktcp/server/persist/Persister.kt
+++
@@ -1,8 +0,0 @@
-package net.kigawa.keruta.ktcp.server.persist
-
-import net.kigawa.keruta.ktcp.model.err.KtcpErr
-import net.kigawa.kodel.api.err.Res
-
-interface Persister {
-    suspend fun createTask(taskToCreate: TaskToCreate): Res<Unit, KtcpErr>
-}
--- ktse/src/main/kotlin/net/kigawa/keruta/ktse/zookeeper/ZkPersisterSession.kt
+++ ktse/src/main/kotlin/net/kigawa/keruta/ktse/zookeeper/ZkPersisterSession.kt
@@ -1,11 +1,8 @@
 package net.kigawa.keruta.ktse.zookeeper
 
-import net.kigawa.keruta.ktcp.server.auth.Verified
 import net.kigawa.keruta.ktcp.server.persist.AuthenticatedPersisterSession
 import net.kigawa.keruta.ktcp.server.persist.PersisterSession
 import net.kigawa.keruta.ktcp.server.session.AuthenticatedSession
-import net.kigawa.keruta.ktse.KtseConfig
-import org.apache.zookeeper.data.Id
 
 class ZkPersisterSession(
     val persister: ZkPersister,
