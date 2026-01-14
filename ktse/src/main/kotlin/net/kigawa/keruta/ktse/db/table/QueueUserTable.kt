e81c79202bb0b7b9e9e612e085fa3e304b063b36
kigawa <mail@kigawa.net>
2026-01-14 17:49:08 +0900

    データベーススキーマを設計し、Exposedを使用したテーブルとマイグレーション初期化処理を追加。Flywayでの自動移行を実装。

---
+++ ktse/src/main/kotlin/net/kigawa/keruta/ktse/db/table/QueueUserTable.kt
@@ -0,0 +1,11 @@
+package net.kigawa.keruta.ktse.db.table
+
+import org.jetbrains.exposed.sql.Table
+import org.jetbrains.exposed.sql.kotlin.datetime.datetime
+
+object QueueUserTable: Table("queue_user") {
+    val queueId = integer("queue_id").references(QueueTable.id)
+    val userId = integer("user_id").references(UserTable.id)
+    val createdAt = datetime("created_at")
+    override val primaryKey = PrimaryKey(queueId, userId)
+}
