f554f39e5bbfaa44f884eb5f559211918b03a042
kigawa <mail@kigawa.net>
2026-02-18 16:09:05 +0900

    wp

--- ktcl-front/app/services/MessageRouterService.ts
+++ ktcl-front/app/services/MessageRouterService.ts
@@ -1,22 +1,23 @@
-import { TaskService, QueueService, ProviderService } from "./domain";
+import {ProviderService, QueueService, TaskService} from "./domain";
 
 export interface MessageRouterDependencies {
-  ws: WebSocket;
-  taskService: TaskService;
-  queueService: QueueService;
-  providerService: ProviderService;
+    ws: WebSocket;
+    taskService: TaskService;
+    queueService: QueueService;
+    providerService: ProviderService;
 }
 
 /**
  * WebSocketメッセージハンドラを作成する
  */
 export function createMessageHandler(deps: MessageRouterDependencies): (event: MessageEvent) => void {
-  const { taskService, queueService, providerService } = deps;
+    const {taskService, queueService, providerService} = deps;
 
-  return (event: MessageEvent) => {
-    const msg = JSON.parse(event.data);
-    taskService.handleMessage(msg);
-    queueService.handleMessage(msg);
-    providerService.handleMessage(msg);
-  };
+    return (event: MessageEvent) => {
+        const msg = JSON.parse(event.data);
+        console.debug("Received message:", msg);
+        taskService.handleMessage(msg);
+        queueService.handleMessage(msg);
+        providerService.handleMessage(msg);
+    };
 }
