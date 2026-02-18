aaf4a1679501f27dc5a92bb405f3fc16c7b76561
kigawa <mail@kigawa.net>
2026-02-18 13:09:11 +0900

    wp

--- ktcl-front/app/Config.ts
+++ ktcl-front/app/Config.ts
@@ -3,4 +3,12 @@ const Config = {
     userIssuer: import.meta.env.VITE_USER_ISSUER,
     userClientId: import.meta.env.VITE_USER_CLIENT_ID,
 } as const
+
+function validate() {
+    for (const key in Config) {
+        if (Config[key] === undefined) throw new Error(`undefined env: ${key}`)
+    }
+}
+
+validate()
 export default Config
