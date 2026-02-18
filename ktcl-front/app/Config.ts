a7737835856812d388522a3dea7a29f2748c1560
kigawa <mail@kigawa.net>
2026-02-18 12:12:00 +0900

    wp

--- ktcl-front/app/Config.ts
+++ ktcl-front/app/Config.ts
@@ -2,6 +2,5 @@ const Config = {
     websocketUrl: new URL(import.meta.env.VITE_WEBSOCKET_URL),
     userIssuer: import.meta.env.VITE_USER_ISSUER,
     userClientId: import.meta.env.VITE_USER_CLIENT_ID,
-    ownIssuer: import.meta.env.VITE_OWN_ISSUER,
 } as const
 export default Config
