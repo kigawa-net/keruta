b8e77327f994357f45da1a42cffaff55dc5f6743
kigawa <mail@kigawa.net>
2026-02-20 09:44:48 +0900

    wp

--- ktcl-front-mobile/src/jsMain/kotlin/net/kigawa/keruta/ktcl/mobile/connection/MobileWebSocketConnection.js.kt
+++ ktcl-front-mobile/src/jsMain/kotlin/net/kigawa/keruta/ktcl/mobile/connection/MobileWebSocketConnection.js.kt
@@ -1,15 +1,16 @@
 package net.kigawa.keruta.ktcl.mobile.connection
 
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+
 /**
  * JS用のMobileWebSocketConnectionスタブ実装
  * モバイル専用プロジェクトのため、JSターゲットでは使用しない
  */
 actual class MobileWebSocketConnection {
-    actual suspend fun send(msg: String) {
-        throw NotImplementedError("JSターゲットではMobileWebSocketConnectionは使用できません")
-    }
+    actual val messages: SharedFlow<String> = MutableSharedFlow()
 
-    actual suspend fun receive(): String? {
+    actual suspend fun send(msg: String) {
         throw NotImplementedError("JSターゲットではMobileWebSocketConnectionは使用できません")
     }
 
