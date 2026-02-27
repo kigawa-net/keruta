d6c41b6c316e59866363828c563a9cd00f1ef7f4
kigawa <mail@kigawa.net>
2026-02-28 01:17:07 +0900

    プロバイダー追加メッセージから不要な`audience`フィールドを削除

--- ktcp/model/src/commonMain/kotlin/net/kigawa/keruta/ktcp/model/provider/add/ServerProviderAddMsg.kt
+++ ktcp/model/src/commonMain/kotlin/net/kigawa/keruta/ktcp/model/provider/add/ServerProviderAddMsg.kt
@@ -9,7 +9,6 @@ data class ServerProviderAddMsg(
     override val type: ServerMsgType = ServerMsgType.PROVIDER_ADD,
     val name: String,
     val issuer: String,
-    val audience: String,
 ) : ServerMsg {
     init {
         require(type == ServerMsgType.PROVIDER_ADD) { "type must be PROVIDER_ADD" }
