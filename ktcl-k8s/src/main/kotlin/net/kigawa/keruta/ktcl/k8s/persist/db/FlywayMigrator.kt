d25bd1314a6da082af208093896ebb7ee4561450
kigawa <mail@kigawa.net>
2026-03-03 22:17:28 +0900

    Flywayマイグレーションファイルをリネームし、新しい命名規則に従って置き換え

--- ktcl-k8s/src/main/kotlin/net/kigawa/keruta/ktcl/k8s/persist/db/FlywayMigrator.kt
+++ ktcl-k8s/src/main/kotlin/net/kigawa/keruta/ktcl/k8s/persist/db/FlywayMigrator.kt
@@ -19,7 +19,7 @@ class FlywayMigrator {
 
         return Flyway.configure()
             .dataSource(jdbcUrl, username, password)
-            .locations("classpath:/db/migration")
+            .locations("classpath:db/migration")
             .sqlMigrationPrefix("V")
             .sqlMigrationSeparator("__")
             .sqlMigrationSuffixes(".sql")
