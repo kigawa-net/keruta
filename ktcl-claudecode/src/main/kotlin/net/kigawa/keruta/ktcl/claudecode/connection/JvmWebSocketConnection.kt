4517c7ceec6ad6a3d1692906f43c460bb3f8acb9
kigawa <mail@kigawa.net>
2026-03-15 16:46:11 +0900

    `TaskReceiver`の受信処理を`break`に変更し、接続終了時のループ停止を適切化。また、`JvmWebSocketConnection`で`ClosedReceiveChannelException`をハンドリングし、安全な受信処理を実装。さらにIDEアーティファクト設定を修正し、モジュール出力を追加。

--- ktcl-claudecode/src/main/kotlin/net/kigawa/keruta/ktcl/claudecode/connection/JvmWebSocketConnection.kt
+++ ktcl-claudecode/src/main/kotlin/net/kigawa/keruta/ktcl/claudecode/connection/JvmWebSocketConnection.kt
@@ -2,6 +2,7 @@ package net.kigawa.keruta.ktcl.claudecode.connection
 
 import io.ktor.client.plugins.websocket.*
 import io.ktor.websocket.*
+import kotlinx.coroutines.channels.ClosedReceiveChannelException
 import net.kigawa.keruta.ktcp.client.KtcpConnection
 
 class JvmWebSocketConnection(
@@ -12,9 +13,13 @@ class JvmWebSocketConnection(
     }
 
     suspend fun receive(): String? {
-        return when (val frame = session.incoming.receive()) {
-            is Frame.Text -> frame.readText()
-            else -> null
+        return try {
+            when (val frame = session.incoming.receive()) {
+                is Frame.Text -> frame.readText()
+                else -> null
+            }
+        } catch (_: ClosedReceiveChannelException) {
+            null
         }
     }
 
