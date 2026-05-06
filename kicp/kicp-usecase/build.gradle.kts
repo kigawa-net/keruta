9e461513ca3cc61e485afe12c5068a3e75cb325a
kigawa <mail@kigawa.net>
2026-05-07 01:06:11 +0900

    feat: kicpモジュールのJS出力設定を追加

--- kicp/kicp-usecase/build.gradle.kts
+++ kicp/kicp-usecase/build.gradle.kts
@@ -4,6 +4,17 @@ plugins {
     id("serialize")
 }
 kotlin {
+    js {
+        binaries.library()
+        generateTypeScriptDefinitions()
+        compilerOptions {
+            moduleKind = org.jetbrains.kotlin.gradle.dsl.JsModuleKind.MODULE_ES
+        }
+        compilations["main"].packageJson {
+            customField("type", "module")
+            customField("types", "keruta-kicp-kicp-usecase.d.mts")
+        }
+    }
     sourceSets["commonMain"].dependencies {
         api(project(":kicp:kicp-domain"))
     }
